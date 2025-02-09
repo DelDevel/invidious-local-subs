ad9c63ac4721ac3f05fa26e635a4bbb952865719
mthsk <mthsk@noreply.codeberg.org>
2025-02-09 23:25:14 +0000

    Update whatsapp-fullscreen/whatsapp-fullscreen.user.js

--- whatsapp-fullscreen/whatsapp-fullscreen.user.js
+++ whatsapp-fullscreen/whatsapp-fullscreen.user.js
@@ -24,8 +24,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-// ==/UserScript==
 (function() {
     "use strict";
     function applyStyles(targetElement) {
